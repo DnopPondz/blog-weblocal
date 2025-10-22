ca82adfecc730c8e3b4a306d44c612a63f137b59
wachirodol Thongnate <166611378+DnopPondz@users.noreply.github.com>
2025-10-22 14:56:48 +0700

    Add footer layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact"
@@ -10,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <div className="pt-20 px-6">
+      <div className="px-6 pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Blog" element={<Blog />} />
@@ -18,7 +19,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
-    
+      <Footer />
     </BrowserRouter>
   );
 }
